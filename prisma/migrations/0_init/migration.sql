45ecad5fb1b9e06e0dfbf1173dac6e283f6faf15
Kevin Liu <kevinxo328@gmail.com>
2025-04-26 14:48:30 +0800

    Feat: reset prisma migration

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -1,10 +1,13 @@
+-- CreateEnum
+CREATE TYPE "Category" AS ENUM ('MENFASHION');
+
 -- CreateTable
 CREATE TABLE "ScrapeKeyword" (
     "id" UUID NOT NULL,
     "keyword" VARCHAR(255) NOT NULL,
     "minPrice" INTEGER,
     "maxPrice" INTEGER,
-    "category" VARCHAR(10) NOT NULL,
+    "category" "Category" NOT NULL DEFAULT 'MENFASHION',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -20,14 +23,30 @@ CREATE TABLE "ScrapeResult" (
     "imageUrl" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "keywordId" UUID NOT NULL,
 
     CONSTRAINT "ScrapeResult_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_ScrapeKeywordToScrapeResult" (
+    "A" UUID NOT NULL,
+    "B" UUID NOT NULL,
+
+    CONSTRAINT "_ScrapeKeywordToScrapeResult_AB_pkey" PRIMARY KEY ("A","B")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "ScrapeKeyword_keyword_key" ON "ScrapeKeyword"("keyword");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "ScrapeResult_url_key" ON "ScrapeResult"("url");
+
+-- CreateIndex
+CREATE INDEX "_ScrapeKeywordToScrapeResult_B_index" ON "_ScrapeKeywordToScrapeResult"("B");
+
 -- AddForeignKey
-ALTER TABLE "ScrapeResult" ADD CONSTRAINT "ScrapeResult_keywordId_fkey" FOREIGN KEY ("keywordId") REFERENCES "ScrapeKeyword"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "_ScrapeKeywordToScrapeResult" ADD CONSTRAINT "_ScrapeKeywordToScrapeResult_A_fkey" FOREIGN KEY ("A") REFERENCES "ScrapeKeyword"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ScrapeKeywordToScrapeResult" ADD CONSTRAINT "_ScrapeKeywordToScrapeResult_B_fkey" FOREIGN KEY ("B") REFERENCES "ScrapeResult"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
