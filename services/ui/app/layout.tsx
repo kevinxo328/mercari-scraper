3462e52e3c8d87e0b82195a341fef5584e6e7403
Kevin Liu <kevinxo328@gmail.com>
2025-04-28 17:57:07 +0800

    Feat: Adds metadata and robots.txt for SEO control
    Introduces metadata for the application, including title and description, and disables search engine indexing. Adds a `robots.txt` file to disallow all crawlers.
    
    Improves control over SEO and privacy settings.

--- services/ui/app/layout.tsx
+++ services/ui/app/layout.tsx
@@ -1,4 +1,14 @@
+import { Metadata } from 'next';
 import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Mercari Scraper',
+  description: 'This website shows the scraped data from Mercari',
+  robots: {
+    index: false,
+    follow: false
+  }
+};
 
 export default function RootLayout({
   children
