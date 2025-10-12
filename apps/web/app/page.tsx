e080d70adfbec33e2ee219067c4098f8facee503
Kevin Liu <kevinxo328@gmail.com>
2025-10-12 16:50:17 +0800

    Feat: enhance main page functionality
    - Updated the getKeywords and getResults procedures to support pagination.
    - Added input parameters for pageSize and page to allow fetching results in a paginated manner.
    - Improved the query logic to skip and take results based on the provided pagination inputs.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,50 +3,76 @@ import TimeDisplay from '@/components/TimeDisplay';
 import { Search } from 'lucide-react';
 import Link from 'next/link';
 import { caller } from '@/trpc/server';
+import React from 'react';
+import { MoveRight } from 'lucide-react';
 
-const LIMIT = 50; // TODO: Make this an environment variable
 export const dynamic = 'force-dynamic';
 
 export default async function Page() {
-  const latestResults = await caller.scraper.getResults({
-    limit: LIMIT,
-    skip: 1
+  const keywords = await caller.scraper.getKeywords({
+    pageSize: 5,
+    page: 1,
+    orderby: 'desc'
   });
 
-  const latestUpdateTime = await caller.scraper
-    .getResults({
-      limit: 1,
-      skip: 1,
+  const promises = keywords.map((keyword) =>
+    caller.scraper.getResults({
+      keywords: [keyword.keyword],
+      pageSize: 12,
+      page: 1,
       orderby: 'desc'
     })
-    .then((results) => results[0]?.updatedAt);
+  );
+
+  const latestResults = await Promise.all(promises);
+
+  const latestUpdateTime = keywords[0]?.updatedAt ?? 'N/A';
 
   return (
     <main className="mx-auto p-4 container relative">
-      <div className="flex justify-between items-center mb-4">
-        <div className="flex flex-col md:flex-row md:gap-2 md: items-baseline">
-          <h4 className="text-xl md:text-3xl font-semibold">Last Updated</h4>
-          <TimeDisplay
-            timestamp={latestUpdateTime}
-            className="text-xs md:text-sm dark:text-gray-400"
-          />
+      <div className="flex justify-end items-center mb-4">
+        <div className="flex flex-col md:flex-row md:gap-2 md:items-baseline text-xs md:text-sm dark:text-gray-400">
+          <span>Last Updated</span>
+          <TimeDisplay timestamp={latestUpdateTime} />
         </div>
-        <Link href="/search" className="flex items-center gap-2">
+        {/* <Link href="/search" className="flex items-center gap-2">
           <Search className="size-4" /> Search
-        </Link>
+        </Link> */}
       </div>
-      <div className="grid grid-cols-2 min-[400px]:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 gap-6">
-        {latestResults.map((result) => (
-          <ScraperResultCard
-            key={result.id}
-            url={result.url}
-            title={result.title}
-            imageUrl={result.imageUrl}
-            price={result.price}
-            currency={result.currency}
-          />
+      {latestResults.length === 0 && (
+        <p className="text-center text-gray-500">No results found</p>
+      )}
+      {latestResults.length > 0 &&
+        latestResults.map((results, index) => (
+          <React.Fragment key={index}>
+            <div className="flex justify-between align-baseline mt-8 mb-2">
+              <h5 className="text-xl font-semibold">
+                {keywords[index]?.keyword}
+              </h5>
+              <Link
+                href={`/search?keywords=${encodeURIComponent(
+                  keywords[index]?.keyword ?? ''
+                )}`}
+                className="text-sm flex items-center gap-1"
+              >
+                View More
+                <MoveRight className="size-4" />
+              </Link>
+            </div>
+            <div className="grid grid-cols-2 min-[400px]:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 gap-6">
+              {results.map((result) => (
+                <ScraperResultCard
+                  key={result.id}
+                  url={result.url}
+                  title={result.title}
+                  imageUrl={result.imageUrl}
+                  price={result.price}
+                  currency={result.currency}
+                />
+              ))}
+            </div>
+          </React.Fragment>
         ))}
-      </div>
     </main>
   );
 }
