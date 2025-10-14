348082fb4b70fa27d710f57b0ebd39d8fa6c8351
Kevin Liu <kevinxo328@gmail.com>
2025-10-14 23:11:59 +0800

    feat: implement client-side data fetching with TRPC

---
+++ apps/web/trpc/server.tsx
@@ -0,0 +1,42 @@
+import 'server-only'; // <-- ensure this file cannot be imported from the client
+import { createContext } from './setup';
+import { appRouter } from './routers/index';
+import {
+  createTRPCOptionsProxy,
+  TRPCQueryOptions
+} from '@trpc/tanstack-react-query';
+import { cache } from 'react';
+import { makeQueryClient } from './shared/query-client';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+
+// IMPORTANT: Create a stable getter for the query client that
+//            will return the same client during the same request.
+export const getQueryClient = cache(makeQueryClient);
+
+export const trpc = createTRPCOptionsProxy({
+  ctx: createContext,
+  router: appRouter,
+  queryClient: getQueryClient
+});
+
+export const caller = appRouter.createCaller(createContext);
+
+export function HydrateClient(props: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      {props.children}
+    </HydrationBoundary>
+  );
+}
+
+export function prefetch<T extends ReturnType<TRPCQueryOptions<any>>>(
+  queryOptions: T
+) {
+  const queryClient = getQueryClient();
+  if (queryOptions.queryKey[1]?.type === 'infinite') {
+    void queryClient.prefetchInfiniteQuery(queryOptions as any);
+  } else {
+    void queryClient.prefetchQuery(queryOptions);
+  }
+}
