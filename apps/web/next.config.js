0df4cbf8e7a862698c6ade8073dceab68b825a6c
Kevin Liu <kevinxo328@gmail.com>
2026-03-07 17:20:02 +0800

    test: remove prisma plugin from next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,17 +1,8 @@
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
-
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
-
-    return config;
-  },
   images: {
     remotePatterns: [
       {
