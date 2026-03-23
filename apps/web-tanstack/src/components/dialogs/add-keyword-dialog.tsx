f52853b882881a53287d9d9f4190c3a6f388903e
Kevin Liu <kevinxo328@gmail.com>
2026-03-23 15:12:03 +0800

    revert(add-keyword-dialog): restore auto-focus on open

--- apps/web-tanstack/src/components/dialogs/add-keyword-dialog.tsx
+++ apps/web-tanstack/src/components/dialogs/add-keyword-dialog.tsx
@@ -180,7 +180,7 @@ export default function AddKeywordDialog({
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>{children}</DialogTrigger>
-      <DialogContent onOpenAutoFocus={(e) => e.preventDefault()}>
+      <DialogContent>
         <DialogHeader>
           <DialogTitle>
             {isEditMode ? 'Edit Search Keyword' : 'Add Search Keyword'}
